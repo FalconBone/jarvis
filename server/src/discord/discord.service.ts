b7f18c12220b1a8176fefe198ca8f41c8909ab88
unknown <happycatvasiliy9786@gmail.com>
2026-04-07 19:19:28 +0400

    changes

--- server/src/discord/discord.service.ts
+++ server/src/discord/discord.service.ts
@@ -16,25 +16,42 @@ export class DiscrodService {
     this.client = new Client({ 
       intents: [
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildVoiceStates
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        // GatewayIntentBits.DirectMessages,
+        // GatewayIntentBits.DirectMessageTyping,
+        // GatewayIntentBits.DirectMessageReactions
       ]
     });
-
-    console.log(process.env.DISCORD_TOKEN)
   }
 
   async onModuleInit() {
     this.client.on("voiceStateUpdate", (oldState, newState) => {
-
+      //если state.channel null, то до этого user не находился ни в одном канале
       if (!oldState.channel && newState.channel) {
-
         console.log(
-          `${newState.member?.user.username} зашел в ${newState.channel.name}`
+          `${newState.member?.user.username} зашел в голосовой канал "${newState.channel.name}"`
         );
-
       }
 
+      if (oldState.channel && !newState.channel) {
+        console.log(
+          `${newState.member?.user.username} вышел из голосового канала "${oldState.channel.name}"`
+        );
+      }
+
+      if (oldState.channel && newState.channel) {
+        console.log(
+          `${newState.member?.user.username} перешел из голосового канала "${oldState.channel.name}" в голосовой канал "${newState.channel.name}"`
+        );
+      }
     });
+
+    this.client.on('messageCreate', (message) => {
+      console.log(message.content)
+    })
 
     this.client.on(Events.ClientReady, readyClient => {
       console.log(`Logged in as ${readyClient.user.tag}!`);
